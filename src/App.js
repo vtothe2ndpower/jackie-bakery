795701418e12b4b27f4ab24b3d5780d00434b1ed
Victor Vargas <vvargasnunez@gmail.com>
2021-01-10 13:13:38 -0800

    Initial commit, set up routes for  basic navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, Switch, NavLink } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Products from './components/Products';
+import Contact from './components/Contact';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <nav className="App-nav">
+          <NavLink exact activeClassName="active-link" to="/">Jackie's Bakery</NavLink>
+          <NavLink exact activeClassName="active-link" to="/about">About</NavLink>
+          <NavLink exact activeClassName="active-link" to="/products">Products</NavLink>
+          <NavLink exact activeClassName="active-link" to="/contact">Contact</NavLink>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/products" component={Products} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Products.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Products extends Component {
+  render() {
+    return (
+      <div>
+        <h1>THIS IS THE PRODUCTS PAGE!</h1>
+      </div>
+    )
+  }
+}
+
+export default Products;
